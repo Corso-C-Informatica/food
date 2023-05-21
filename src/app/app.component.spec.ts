0543b5e881768e1d0a4a160608cff456fc9c55e6
Yrnmama <mamadou.gningue@marconirovereto.it>
2023-05-21 20:53:01 +0000

    COMMIT

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tcfoodcorner'`, () => {
+  it(`should have as title 'ordini ritorante '`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tcfoodcorner');
+    expect(app.title).toEqual('oridni ritorante ');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tcfoodcorner app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ordini ritorante funziona ');
   });
 });
